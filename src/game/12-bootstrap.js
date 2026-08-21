9a74dd35c31af1bc9e24672dd30544a7a92ea1ef
amazin20 <138931938+amazin20@users.noreply.github.com>
2026-08-21 14:33:16 +0300

    Import game bootstrap

---
+++ src/game/12-bootstrap.js
@@ -0,0 +1,3 @@
+/* -------------------------------- bootstrap -------------------------------- */
+addEnvironment();addLabObjects();player.yaw=Math.PI/2;player.forward=V.v(1,0,0);updateChecks();initYandex();requestAnimationFrame(loop);
+
